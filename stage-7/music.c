22cdf507b72205a94d08eba7f9cc0bb35e4766cd
abois526 <abois526@mtroyal.ca>
2024-04-02 12:28:46 -0600

    kinda got timing going

--- stage-7/music.c
+++ stage-7/music.c
@@ -27,7 +27,7 @@
 #define TICKS_PER_Q_NOTE (TICKS_PER_W_NOTE / 4)
 #define TICKS_PER_E_NOTE (TICKS_PER_W_NOTE / 8)
 
-UINT32 curr_note_index;
+UINT32 curr_note_index = 0;
 UINT32 time_since_last_note = 0;
 
 /*---------- FUNCTION: start_music ------------------------
@@ -48,7 +48,6 @@ UINT32 time_since_last_note = 0;
 /--------------------------------------------------------*/
 void start_music()
 {
-    curr_note_index = 0;
 
     set_tone(0, channel_a[curr_note_index].pitch);
     set_volume(0, channel_a[curr_note_index].level);
@@ -76,22 +75,18 @@ void start_music()
 /--------------------------------------------------------*/
 void update_music(UINT32 time_elapsed)
 {    
-    time_since_last_note += time_elapsed;
+    if(time_elapsed % 20 == 0) {
 
-    if(time_elapsed > 0) {
-        if(time_since_last_note >= TICKS_PER_Q_NOTE) {
-    
-            stop_sound();
-            set_tone(0, channel_a[curr_note_index].pitch);
-            set_volume(0, channel_a[curr_note_index].level);
-            enable_channel(0, 1, 0);
-            set_envelope(channel_a[curr_note_index].env_shape,
-                            channel_a[curr_note_index].env_sustain);
-            
-            /* making sure it wraps back around */
-            curr_note_index = (curr_note_index + 1) % SONG_LENGTH; 
-            time_since_last_note = 0;
-        }
+        stop_sound();
+        set_tone(0, channel_a[curr_note_index].pitch);
+        set_volume(0, channel_a[curr_note_index].level);
+        enable_channel(0, 1, 0);
+        set_envelope(channel_a[curr_note_index].env_shape,
+                        channel_a[curr_note_index].env_sustain);
+        
+        /* making sure it wraps back around */
+        curr_note_index = (curr_note_index + 1) % SONG_LENGTH; 
+        time_since_last_note = 0;
     }
 
     
