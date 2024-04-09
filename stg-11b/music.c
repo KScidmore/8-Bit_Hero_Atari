58b2c06ca22518c4c8c813887d1972666c646077
Kyle Scidmore <kscid125@mtroyal.ca>
2024-04-08 22:37:24 -0600

    stage 11b added

--- stg-11b/music.c
+++ stg-11b/music.c
@@ -30,28 +30,26 @@
 UINT32 curr_note_index = 0;
 UINT32 time_since_last_note = 0;
 
-/*---------- FUNCTION: start_music ------------------------
+/*---------- FUNCTION: update_music -----------------------
 /  PURPOSE:
-/    TODO - purpose, from the caller's perspective (if not 
-/    perfectly clear from the name)
+/    Updates main game song
 /  
 /  CALLER INPUT:
-/    TODO - the purpose of each input parameter (if not 
-/    perfectly clear from the name)
+/    UINT32 total_time_elapsed
+/      -Total time the game has been running
 /  
 /  CALLER OUTPUT:
-/    TODO - the purpose of each output parameter and return 
-/    value (if not perfectly clear from the name)
+/    N/A
 /  
 /  ASSUMPTIONS, LIMITATIONS, AND KNOWN BUGS:
-/    TODO 
+/    N/A
 /--------------------------------------------------------*/
 void start_music()
 {
 
     set_tone(0, channel_a[curr_note_index].pitch);
-    set_volume(0, channel_a[curr_note_index].level);
     enable_channel(0, 1, 0);
+    set_volume(0, channel_a[curr_note_index].level);
     set_envelope(channel_a[curr_note_index].env_shape, 
                  channel_a[curr_note_index].env_sustain);
 
@@ -75,23 +73,17 @@ void start_music()
 /  ASSUMPTIONS, LIMITATIONS, AND KNOWN BUGS:
 /    TODO 
 /--------------------------------------------------------*/
-void update_music(UINT32 total_time_elapsed, int count)
+void update_music()
 {   
-    if(count >= 343){
 
-        if(total_time_elapsed % 69 == 0) {
+        stop_sound_channel_a();
+        set_tone(0, channel_a[curr_note_index].pitch);
+        enable_channel(0, 1, 0);
+        set_volume(0, channel_a[curr_note_index].level);
+        set_envelope(channel_a[curr_note_index].env_shape,
+                        channel_a[curr_note_index].env_sustain);
 
-            stop_sound();
-            set_tone(0, channel_a[curr_note_index].pitch);
-            set_volume(0, channel_a[curr_note_index].level);
-            enable_channel(0, 1, 0);
-            set_envelope(channel_a[curr_note_index].env_shape,
-                            channel_a[curr_note_index].env_sustain);
-
-            /* making sure it wraps back around */
-            curr_note_index = (curr_note_index + 1) % SONG_LENGTH; 
-        }
-
-    }
+        /* making sure it wraps back around */
+        curr_note_index = (curr_note_index + 1) % SONG_LENGTH; 
     
 }
